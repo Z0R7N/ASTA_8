6d2287c96e9ccf094804c80c5a460622a779050e
Z0R7N <46117076+Z0R7N@users.noreply.github.com>
2021-04-13 10:21:22 +0300

    create steps for Payment page

--- src/test/java/steps/BasePageDef.java
+++ src/test/java/steps/BasePageDef.java
@@ -1,5 +1,23 @@
 package steps;
 
-public class BasePageDef {
+import io.cucumber.java.en.Then;
+import pages.BasePage;
 
+public class BasePageDef {
+    BasePage basePage = new BasePage();
+
+    @Then("Click link {string}")
+    public void clickLink(String arg0) {
+        basePage.clickLink(arg0);
+    }
+
+    @Then("Click button {string}")
+    public void clickButton(String arg0) {
+        basePage.clickButton(arg0);
+    }
+
+    @Then("Check content {string}")
+    public void checkContent(String arg0) {
+        basePage.checkContent(arg0);
+    }
 }
--- src/test/java/steps/PaymentPageDef.java
+++ src/test/java/steps/PaymentPageDef.java
@@ -1,4 +1,39 @@
 package steps;
 
+import config.UserConfig;
+import io.cucumber.java.en.Then;
+import pages.PaymentPage;
+
 public class PaymentPageDef {
+    PaymentPage paymentPage = new PaymentPage();
+
+    @Then("Choose card")
+    public void chooseCard() {
+        paymentPage.chooseComboBox("task8_form_cardType", UserConfig.CARDTYPE);
+    }
+
+    @Then("Enter name and family")
+    public void enterNameAndFamily() {
+        paymentPage.enterText("task8_form_name", UserConfig.NAME + " " + UserConfig.FAMILY);
+    }
+
+    @Then("Enter card number")
+    public void enterCardNumber() {
+        paymentPage.enterText("task8_form_cardNumber", UserConfig.CARD_NUMBER);
+    }
+
+    @Then("Enter CVV number")
+    public void enterCVVNumber() {
+        paymentPage.enterText("task8_form_cardCvv", UserConfig.CVV_NUMBER);
+    }
+
+    @Then("Choose month {string}")
+    public void chooseMonth(String arg0) {
+        paymentPage.chooseComboBox("task8_form_cardInfo_month", arg0);
+    }
+
+    @Then("Choose year {string}")
+    public void chooseYear(String arg0) {
+        paymentPage.chooseComboBox("task8_form_cardInfo_year", arg0);
+    }
 }
